6183767edbf37ef1c751ada0ea9910c7700338ce
Minhaz <sharif35-3001@diu.edu.bd>
2023-11-13 10:47:48 +0600

    feat: organization enabled

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,18 +1,44 @@
-"use client";
-
-import { UserButton, useAuth, useClerk } from "@clerk/nextjs";
+import {
+	OrganizationSwitcher,
+	SignOutButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-	const { isSignedIn, isLoaded } = useAuth();
-	const { signOut } = useClerk();
-	const router = useRouter();
-
 	return (
 		<nav>
-			Navbar: {isLoaded && isSignedIn ? <UserButton /> : <Link href="/sign-in">Login</Link>}
-			<button onClick={() => signOut(() => router.push("/sign-in"))}>Sign out</button>
+			Initial Navbar information:
+			<div>
+				<SignedIn>
+					<SignOutButton>
+						<div className="flex cursor-pointer">
+							<Image
+								src="/assets/icons/logout.svg"
+								alt="logout"
+								width={24}
+								height={24}
+								className="invert brightness-100"
+							/>
+						</div>
+					</SignOutButton>
+
+					<UserButton />
+					<OrganizationSwitcher
+						appearance={{
+							elements: {
+								organizationSwitcherTrigger: "py-2 px-4",
+							},
+						}}
+					/>
+				</SignedIn>
+				<SignedOut>
+					<Link href="/sign-in">Login</Link>
+				</SignedOut>
+			</div>
 		</nav>
 	);
 }
